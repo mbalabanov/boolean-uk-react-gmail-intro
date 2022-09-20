2ab154f426d20baadf9ed96ecb49f47bba9b4ae9
mbalabanov <marincomics@gmail.com>
2022-09-20 18:45:43 +0200

    Extracts into more components

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,6 +6,7 @@ function Nav() {
           <span className="label">Inbox</span>
           <span className="count">3</span>
         </li>
+
         <li className="item">
           <span className="label">Starred</span>
           <span className="count">2</span>
